d01d3e4a360bb4fb7c231366428bddb09bf4f7f6
ZeceL <165992809+ZeceL@users.noreply.github.com>
2024-11-20 10:04:26 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uch_practica
+{
+    public class Book
+    {
+        // свойства книги
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearPublished{ get; private set; }
+
+        //конструктор класса
+        public Book (string id, string title, string author, int yearPublished) 
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            YearPublished = yearPublished;
+        }
+    }
+}
---
+++ BookManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uch_practica
+{
+    public class BookManager
+    {
+        // свойство класса
+        private List<Book> Books {  get; set; }
+
+        // конструктор класса
+        public BookManager() 
+        {
+            Books = new List<Book>();
+        }
+
+        // метод добавления книги
+        public void AddBook(Book book)
+        { 
+            if (Books.Any(b => b.Id == book.Id)) 
+            {
+                throw new InvalidOperationException("Книга с таким Id уже существует."); 
+            }
+            Books.Add(book);
+        }
+
+        // метод удаления книги
+        public void RemoveBook(string bookId)
+        {
+            var book = GetBook(bookId);
+            if (book == null)
+                throw new KeyNotFoundException("Книга с таким Id не найдена");
+            Books.Remove(book);
+        }
+
+        // метод получения книги по Id
+        public Book GetBook(string bookId)
+        {
+            return  Books.FirstOrDefault (b=> b.Id == bookId); 
+        }
+
+        // метод получения спика всех книг
+        List<Book> GetAllBooks() 
+        {
+            return new List<Book>(Books); 
+        }
+    }
+}
